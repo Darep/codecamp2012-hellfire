a8efb23140ee92a7c789ef54b67040a8254226a6
Antti-Jussi Kovalainen <dareppi@gmail.com>
2012-11-16 13:25:20 +0200

    Add actual real require.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-require(function () {
+require.config({
+    shim: {
+        'lib/soundmanager2': [],
+        'lib/slickgrid/slick.grid': ['lib/slickgrid/jquery.event.drag-2.0.min', 'lib/slickgrid/slick.core', 'lib/slickgrid/slick.dataview', 'lib/slickgrid/plugins/slick.rowselectionmodel'],
+        'beatstream/audio-modules/soundmanager2': ['lib/soundmanager2']
+    },
 
+    paths: {
+        //'lib/soundmanager2': 'lib/soundmanager2-nodebug-jsmin'
+        'lib/store': 'lib/store.min',
+        'lib/jquery-ui': 'lib/jquery-ui-1.8.17.custom.min'
+    },
+
+    urlArgs: "timestamp=" + new Date().getTime(),
+
+    // We will bail out instantly if there is error loading any script
+    // (default is 7 seconds -> causes you to stare empty console)
+    waitSeconds: 1
 });
+
+require(
+    [],
+    function () {
+        console.log('asdf');
+    }
+);

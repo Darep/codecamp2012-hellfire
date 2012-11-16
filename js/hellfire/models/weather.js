d7ebb7eda0779db70c6d51c16c9572a846a9c004
Jouni Honkala <jouni.honkala@iki.fi>
2012-11-16 17:29:51 +0200

    Calculation year stored to model/weather.js

--- js/hellfire/models/weather.js
+++ js/hellfire/models/weather.js
@@ -1,5 +1,5 @@
-define(['jquery', 'backbone', 'hellfire/service/weather'], 
-function($, Backbone, WeatherService){
+define(['jquery', 'backbone', 'hellfire/service/weather', 'hellfire/utils/calculator'], 
+function($, Backbone, WeatherService, Calculator){
 
     var Model = Backbone.Model.extend({
 
@@ -7,17 +7,21 @@ function($, Backbone, WeatherService){
             weather: 'sunny',
             city: 'Tampere',
             cityLabel: 'Tampere',
+            calculationYear: 3012,
             temperature: "23 'C"
         },
 
         initialize: function () {
             this.on("change:city", this.refreshData, this);
+            this.on("change:calculationYear", this.refreshData, this);
         },
 
         refreshData: function () {
             var city = this.get("city");
+            var calcYear = this.get("calculationYear");
             this.set("weather", WeatherService.getWeather(city));
-            this.set("temperature", WeatherService.getTemperature(city)+"' C");
+            var temperature = WeatherService.getTemperature(city);
+            this.set("temperature", Calculator.calculate(temperature, calcYear)+"' C");
             this.set("cityLabel", WeatherService.getCityByKey(city).name);
         }
     });
